eb051ac892180608ee862730b28767dcefabc687
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-01-24 15:57:00 -0300

    atual

--- Caelumnos/src (opcional)/js/listaAluno.js
+++ Caelumnos/src (opcional)/js/listaAluno.js
@@ -3,15 +3,15 @@
     $formNovoAluno.on('novoAluno', function (evento) {
         const novoAluno = evento.detail.novoAluno;
 
-        const tabelaListagemDeAlunos = $('#listagemDeAlunos');
-        const elementoNovoAluno = $(`
+        const $tabelaListagemDeAlunos = $('#listagemDeAlunos');
+        const $elementoNovoAluno = $(`
             <tr>
                 <td>${novoAluno.matricula}</td>
                 <td id="nome" contentEditable="false">${novoAluno.nome}</td>
                 <td><button data-type="delete" class="btn btn-danger removeAluno">Excluir</td>
             </tr>
         `);
-        tabelaListagemDeAlunos.append(elementoNovoAluno);
+        $tabelaListagemDeAlunos.append($elementoNovoAluno);
     });
 
     const tabelaDeAlunos = document.querySelector("#listagemDeAlunos");
--- Caelumnos/src/js/formNovoAluno.js
+++ Caelumnos/src/js/formNovoAluno.js
@@ -15,5 +15,7 @@
             </tr>
         `);
         tabelaListagemDeAlunos.append(elementoNovoAluno);
+
+
     })
 })();
