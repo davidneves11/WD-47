30115f87f39358354e237e23301e166beaef00fb
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-02-07 15:19:20 -0300

    Novos Desafios

--- Caelumnos/src/js/eventos.js
+++ Caelumnos/src/js/eventos.js
@@ -4,27 +4,28 @@
     const tabelaDeAlunos = document.querySelector("#listagemDeAlunos");
     tabelaDeAlunos.addEventListener('click', function (event) {
         if (event.target.classList.contains("removeAluno") && confirm("Tem certeza que deseja excluir?")) {
-            event.target.remove();
+            event.target.closest('tr').remove();
         }
     })
 
     // Muda o texto do botão de alterar
     tabelaDeAlunos.addEventListener('click', function (event) {
         if (event.target.classList.contains('btn-warning')) {
-            if (event.target.textContent == "Alterando") {
+            if (event.target.textContent == "Alterando...") {
                 event.target.textContent = "Alterar Nome"
             } else {
-                event.target.textContent = "Alterando"
+                event.target.textContent = "Alterando..."
             }
         }
     })
 
-    // Coloca e tira o atributo contentEditable para o nome do aluno
+    // Coloca e tira o atributo contentEditable para o nome do aluno e foca no campo nome
     tabelaDeAlunos.addEventListener('click', function (event) {
         if (event.target.classList.contains('btn-warning')) {
-            if (event.target.textContent == "Alterando") {
+            if (event.target.textContent == "Alterando...") {
                 const nomeAluno = event.target.closest('.aluno').querySelector('.nomeAluno');
                 nomeAluno.setAttribute("contentEditable", "true");
+                nomeAluno.focus();
             } else {
                 const nomeAluno = event.target.closest('.aluno').querySelector('.nomeAluno');
                 nomeAluno.removeAttribute("contentEditable");
@@ -46,6 +47,26 @@
         }
     })
 
+    // Salva o conteúdo de texto antes da alteração
+    tabelaDeAlunos.addEventListener('focusin', function (event) {
+        if (event.target.classList.contains('nomeAluno')) {
+            window.conteudoSalvo = event.target.textContent;
+        }
+    })
+
+    // Alterar o conteudo de texto do botão "Alterar Nome" ao desfocar do campo nome
+    tabelaDeAlunos.addEventListener('focusout', function (event) {
+        if (event.target.classList.contains('nomeAluno')) {
+            if (event.target.textContent.trim().length == 0) {
+                alert("O aluno precisa de um nome!!!");
+                event.target.textContent = conteudoSalvo;
+                event.target.focus();
+            } else {
+                const btnAltera = event.target.closest('.aluno').querySelector('.btn-warning');
+                btnAltera.textContent = "Alterar Nome";
+            }
+        }
+    })
 
 })();
 
