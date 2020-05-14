c8cc61a9431906e4e0e5c11d334391d577f6cb1e
André Tardelli <andretardelli@gmail.com>
2020-05-14 19:23:38 -0300

    Inserção de mascara de CPF e modal para confirmação normal

--- Caelumnos/src/js/formNovoAluno.js
+++ Caelumnos/src/js/formNovoAluno.js
@@ -1,15 +1,30 @@
-(function () {
-   
-    const form = document.querySelector("#formNovoAluno");
-    let numeroMatricula = 0;
-   
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
 
+const form = document.querySelector("#formNovoAluno");
+const formModal = document.querySelector("#btnSubmit");
+let numeroMatricula = 0;
+
+formModal.addEventListener("click", function (event) {
+
+    if (event.target.classList.contains("btnModal")) {
         numeroMatricula++
 
         const nomeCurso = form.querySelector("#nomeCurso").value;
         const nome = form.querySelector("#nomeAluno").value;
+        const cpf = form.querySelector("#cpf").value;
+
+
+        if (nome == "") {
+            alert("Insira o Nome");
+            return false;
+        }
+        if (nomeCurso == " -- Escolha uma opção -- ") {
+            alert("Insira o Nome");
+            return false;
+        }
+        if (validarCPF(cpf) == false) {
+            alert("CPF invalido!");
+            return false;
+        }
 
         const tabelaListagemDeAlunos = $('#listagemDeAlunos');
         const elementoNovoAluno = $(`
@@ -17,6 +32,7 @@
                     <td class="pesquisa linha--formata">${numeroMatricula}</td>
                     <td class="pesquisa linha--formata nomeAluno">${nome}</td>
                     <td class="pesquisa linha--formata">${nomeCurso}</td>
+                    <td class="pesquisa linha--formata">${cpf}</td>
                     <td>
                         <button type="button" class="btn btn-warning">Alterar Nome</button>
                         <button data-type="delete" class="btn btn-danger removeAluno">Excluir</button>
@@ -28,6 +44,7 @@
 
         form.nomeCurso.value = '';
         form.nomeAluno.value = '';
+        form.cpf.value = '';
+    }
 
-    })
-})();
+})
