cf0a517f35379e18dc1936a2716312332781e495
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-06-22 14:24:50 -0300

    Update

--- Caelumnos/src/js/btnAltera.js
+++ Caelumnos/src/js/btnAltera.js
@@ -1,10 +1,10 @@
-(function () {
-
+;(function () {
+    
     const tabelaDeAlunos = document.querySelector("#listagemDeAlunos");
     
     // Muda o texto do botão de alterar
     tabelaDeAlunos.addEventListener('click', function (event) {
-        if (event.target.classList.contains('btn-warning')) {
+        if (event.target.classList.contains('edita')) {
             if (event.target.textContent == "Alterando...") {
                 event.target.textContent = "Alterar Nome"
             } else {
@@ -15,15 +15,12 @@
 
     // Adiciona e remove o atributo contentEditable para o nome do aluno e foca no campo nome
     tabelaDeAlunos.addEventListener('click', function (event) {
-        if (event.target.classList.contains('btn-warning')) {
-            if (event.target.textContent == "Alterando...") {
+        if (event.target.classList.contains('edita')) {
+                event.target.textContent = "Alterando..."
                 const nomeAluno = event.target.closest('.aluno').querySelector('.nomeAluno');
                 nomeAluno.setAttribute("contentEditable", "true");
                 nomeAluno.focus();
-            } else {
-                const nomeAluno = event.target.closest('.aluno').querySelector('.nomeAluno');
-                nomeAluno.removeAttribute("contentEditable");
-            }
+            
         }
     })
 
@@ -42,9 +39,11 @@
                 event.target.textContent = conteudoSalvo;
                 event.target.focus();
             } else {
-                const btnAltera = event.target.closest('.aluno').querySelector('.btn-warning');
-                btnAltera.innerHTML = "<svg class='bi bi-pencil-square' width='1em' height='1em' viewBox='0 0 16 16' fill='currentColor xmlns='http://www.w3.org/2000/svg'><path d='M15.502 1.94a.5.5 0 010 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 01.707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 00-.121.196l-.805 2.414a.25.25 0 00.316.316l2.414-.805a.5.5 0 00.196-.12l6.813-6.814z'/><path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 002.5 15h11a1.5 1.5 0 001.5-1.5v-6a.5.5 0 00-1 0v6a.5.5 0 01-.5.5h-11a.5.5 0 01-.5-.5v-11a.5.5 0 01.5-.5H9a.5.5 0 000-1H2.5A1.5 1.5 0 001 2.5v11z' clip-rule='evenodd'/></svg>"
+                const btnAltera = event.target.closest('.aluno').querySelector('.edita');
+                btnAltera.innerHTML = "<svg class='bi bi-pencil-square edita' width='1em' height='1em' viewBox='0 0 16 16' fill='currentColor xmlns='http://www.w3.org/2000/svg'><path d='M15.502 1.94a.5.5 0 010 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 01.707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 00-.121.196l-.805 2.414a.25.25 0 00.316.316l2.414-.805a.5.5 0 00.196-.12l6.813-6.814z'/><path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 002.5 15h11a1.5 1.5 0 001.5-1.5v-6a.5.5 0 00-1 0v6a.5.5 0 01-.5.5h-11a.5.5 0 01-.5-.5v-11a.5.5 0 01.5-.5H9a.5.5 0 000-1H2.5A1.5 1.5 0 001 2.5v11z' clip-rule='evenodd'/></svg>" 
+                const nomeAluno = event.target.closest('.aluno').querySelector('.nomeAluno');
+                nomeAluno.removeAttribute("contentEditable");
             }
         }
     })
-})();
+})()
--- Caelumnos/src/js/formNovoAluno.js
+++ Caelumnos/src/js/formNovoAluno.js
@@ -1,9 +1,9 @@
 
 const form = document.querySelector("#formNovoAluno");
-const formModal = document.querySelector("#btnSubmit");
+const btnFormModal = document.querySelector("#btnSubmit");
 let numeroMatricula = 0;
 
-formModal.addEventListener("click", function (event) {
+btnFormModal.addEventListener("click", function (event) {
 
     if (event.target.classList.contains("btnModal")) {
         numeroMatricula++
@@ -16,7 +16,7 @@ formModal.addEventListener("click", function (event) {
             alert("Insira o Nome");
             return false;
         }
-        if (nomeCurso == " -- Escolha uma opção -- ") {
+        if (nomeCurso == "") {
             alert("Insira o Curso");
             return false;
         }
@@ -33,8 +33,8 @@ formModal.addEventListener("click", function (event) {
                     <td class="pesquisa linha--formata">${nomeCurso}</td>
                     <td class="pesquisa linha--formata">${cpf}</td>
                     <td>
-                        <button type="button" class="btn btn-warning" data-toggle="tooltip" data-placement="top" title="Editar">
-                            <svg class="bi bi-pencil-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <button type="button" class="btn btn-warning edita" data-toggle="tooltip" data-placement="top" title="Editar">
+                            <svg class="bi bi-pencil-square edita" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path d="M15.502 1.94a.5.5 0 010 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 01.707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 00-.121.196l-.805 2.414a.25.25 0 00.316.316l2.414-.805a.5.5 0 00.196-.12l6.813-6.814z"/>
                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 002.5 15h11a1.5 1.5 0 001.5-1.5v-6a.5.5 0 00-1 0v6a.5.5 0 01-.5.5h-11a.5.5 0 01-.5-.5v-11a.5.5 0 01.5-.5H9a.5.5 0 000-1H2.5A1.5 1.5 0 001 2.5v11z" clip-rule="evenodd"/>
                             </svg>
